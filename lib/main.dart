8d16291a1418b6012ea9b635bae011ae012ee47c
Chad Ross <rosschad98@icloud.com>
2020-03-03 15:41:17 -0800

    iOS Firebase builds successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() => runApp(MyApp());
 
