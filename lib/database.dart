6477efe997d58787895c6eaa5b48a4187cda41ce
GriffenMarler <55120745+GriffenMarler@users.noreply.github.com>
2020-03-08 16:49:58 -0700

    Commented database.dart file

--- lib/database.dart
+++ lib/database.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'game.dart';
 
+// This class will store the database reference we are using.
 class Database {
   final firestore_db = Firestore.instance;
 
