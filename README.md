7f8ab9bd8a6e76f7281bf5a8c067c5be74ce4cc0
Casey Sweet <sweetca72@gmail.com>
2020-03-08 21:01:03 -0700

    See README for current functioning

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
 # pickup_app
 
-A new Flutter project.
 
-## Getting Started
+This UI currently has address (string), msg (string), sport (string), public or private (string), and number of player(string) all able to be saved.
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Working on getting date/time to properly save currently.
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:numberpicker/numberpicker.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Basic PickUp Game Creator Page'),
     );
   }
 }
@@ -46,16 +50,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  
+  String dropdownsport = "Basketball"; ///initial value of sport
+  String dropdownpub = "Public"; //initial value of priv or pub match
 
-  void _incrementCounter() {
+  final myControllerAddr = TextEditingController(); //for the entered address
+  final myControlMsg = TextEditingController(); // for the entered Message
+  final myControlSport = TextEditingController(); // for sport selection
+  final myControlpub = TextEditingController(); // for pub/priv match selection
+
+  String addr = " ";
+  String msg = " ";
+
+  var gameDate = DateTime.now();
+  final startGameTime = DateTime.now();
+  final endGameTime = DateTime.now();
+
+  final format = DateFormat("yyyy-MM-dd HH:mm"); //for the DateTimePicker
+
+  int _currentNumPlay = 3;
+
+  void _updateData() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      addr = myControllerAddr.text;
+      msg = myControlMsg.text;
     });
   }
 
@@ -67,6 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -77,6 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
+
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -91,22 +116,187 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:()',
+
+///enter address -------------------------------------------------------------
+            Row(
+              children: <Widget>[
+                Container(
+                    child: Text('Address:', style: TextStyle(fontSize: 20),),
+                    constraints: BoxConstraints(maxHeight: 50.0, maxWidth: 100.0, minHeight: 50.0, minWidth: 50.0),
+                    alignment: Alignment.center,),
+                Container(
+                    child: TextField(decoration: InputDecoration( hintText: 'Enter Address Here'), controller: myControllerAddr,),
+                    constraints: BoxConstraints(maxHeight: 50.0, maxWidth: 300.0, minHeight: 50.0, minWidth: 50.0),),], 
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+
+///time and date row ------------------------------------------------------------
+            Row(children: <Widget>[ 
+              Container( //Time/Date text
+                child: Text('Time/Date:', style: TextStyle(fontSize: 20)),
+                constraints: BoxConstraints(maxHeight: 50.0, maxWidth: 100.0, minHeight: 50.0, minWidth: 50.0),
+                alignment: Alignment.bottomCenter,),
+              Container(
+                child: FlatButton( ///date button
+                    onPressed: () {
+                            DatePicker.showDatePicker(context,
+                              showTitleActions: true,
+                              minTime: DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day),
+                              maxTime: DateTime(2020, 12, 31), onChanged: (date) {
+                            print('change $date');
+                            gameDate = date;
+                          }, onConfirm: (date) {
+                            print('confirm $date');
+                          }, currentTime: gameDate, locale: LocaleType.en);
+                      },        
+                          child: Text(
+                              'date',
+                           style: TextStyle(color: Colors.blue),
+                    )),
+                  constraints: BoxConstraints(maxHeight: 50.0, maxWidth: 100.0, minHeight: 50.0, minWidth: 50.0),
+                  alignment: Alignment.center,),
+                Container(
+                  child: FlatButton( ////start time button
+                    onPressed: () {
+                         DateTimeField( //https://pub.dev/packages/datetime_picker_formfield#-readme-tab-
+                           format: format,
+                           onShowPicker: (context, currentValue) async {
+                            final date = await showDatePicker(
+                            context: context,
+                            firstDate: DateTime(DateTime.now().year,DateTime.now().month, DateTime.now().day),
+                            initialDate: currentValue ?? DateTime.now(),
+                            lastDate: DateTime(DateTime.now().year + 2));
+                            if (date != null) {
+                                final time = await showTimePicker(
+                                    context: context,
+                                    initialTime: TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+                                );
+                              return DateTimeField.combine(date, time);
+                            } 
+                            else {
+                             return currentValue;
+                            }
+                          },);
+                        },      
+                          child: Text(
+                              'start time',
+                           style: TextStyle(color: Colors.blue),
+                    )),
+                  constraints: BoxConstraints(maxHeight: 50.0, maxWidth: 100.0, minHeight: 50.0, minWidth: 50.0),
+                  alignment: Alignment.center,)
+                  
+            ],),
+
+// select sport ------------------------------------------------------------------------
+            Row( //sport row
+              children: <Widget>[
+                Container(
+                  child: Text( 'Sport:',style: TextStyle(fontSize: 20), ),
+                  constraints: BoxConstraints(maxHeight: 50.0, maxWidth: 100.0, minHeight: 50.0, minWidth: 50.0),
+                  alignment: Alignment.center,),
+                Container(
+                  child: DropdownButton<String>(
+                            value: dropdownsport,
+                            iconSize: 20,
+                            onChanged: (String newValue){
+                            setState(() {
+                               dropdownsport = newValue;
+                            });
+                            },
+                            items: <String> ['Basketball', 'Soccer', 'Football', 'Baseball']
+                            .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(value: value, child: Text(value),);
+                             }).toList(),
+                      ),
+                  alignment: Alignment.centerRight,
+                 )
+              ]),
+
+// private or public match ----------------------------------------------------------
+            Row( //private or public match row
+              children: <Widget>[
+                Container(
+                  child: Text("Private or Public match?", style: TextStyle(fontSize: 20)),
+                  constraints: BoxConstraints(maxHeight: 50.0, maxWidth: 200.0, minHeight: 50.0, minWidth: 50.0),
+                  alignment: Alignment.center,
+                  ),
+                Container(
+                  alignment: Alignment.center,
+                  child: DropdownButton<String>(
+                      value: dropdownpub,
+                      iconSize: 20,
+                      onChanged: (String newValue){
+                       setState(() {
+                          dropdownpub = newValue;
+                       });
+              },
+              items: <String> ['Private', 'Public']
+                .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(value: value, child: Text(value),);
+                }).toList(),
+                ),)
+              ],
             ),
+           
+///# of players ---------------------------------------------------------------------------------
+            Row(children: <Widget>[
+              Container(alignment: Alignment.centerLeft,
+                child: Text('# of players: ', style: TextStyle(fontSize: 20),),
+                constraints: BoxConstraints(maxHeight: 50.0, maxWidth: 200.0, minHeight: 50.0, minWidth: 50.0),),
+              Container(alignment: Alignment.center,
+                child: new NumberPicker.integer(
+                  initialValue: _currentNumPlay,
+                  minValue: 3,
+                  maxValue: 25,
+                  onChanged:(newValue) =>
+                    setState(() => _currentNumPlay = newValue))
+                )
+            ],),
+
+
+ ///any addition commments
+            TextField(decoration: InputDecoration( hintText: 'Anything else to note:'), controller: myControlMsg,),
+
+
+////text to show the entered information
+            Text('addr: $addr'),
+            Text('msg: $msg'),
+            Text('sport: $dropdownsport'),
+            Text('pub/priv: $dropdownpub'),
+            Text('num of players: $_currentNumPlay'),
+            Text('Basic date & time field (${format.toString()})'),
+
+ ///Another way of selecting the time
+            DateTimeField( //https://pub.dev/packages/datetime_picker_formfield#-readme-tab-
+                  format: format,
+                   onShowPicker: (context, currentValue) async {
+                    final date = await showDatePicker(
+                    context: context,
+                   firstDate: DateTime(DateTime.now().year,DateTime.now().month, DateTime.now().day),
+                   initialDate: currentValue ?? DateTime.now(),
+                    lastDate: DateTime(DateTime.now().year + 2));
+                    if (date != null) {
+                   final time = await showTimePicker(
+                    context: context,
+                    initialTime:
+                      TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+                    );
+                    return DateTimeField.combine(date, time);
+                } else {
+                  return currentValue;
+          }
+        },
+      ),
           ],
         ),
       ),
+
+// the submit button ------------------------------------------------------------
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _updateData,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Text('Submit'),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
