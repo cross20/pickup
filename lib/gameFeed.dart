ff77a8f1d26a180200504e68d83f976822d4dd41
Chad Ross <rosschad98@icloud.com>
2020-03-30 17:13:49 -0700

    Fix for issue which caused the incorrect number of games to load.

--- lib/gameFeed.dart
+++ lib/gameFeed.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+
 class GameFeed extends StatefulWidget {
   GameFeed({Key key, this.title}) : super(key: key);
 
