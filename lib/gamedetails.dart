e620ee15e1b6e592be33fefe8e2186f6d2b1b034
GriffenMarler <gmarler20@my.whitworth.edu>
2020-04-26 11:16:23 -0700

    Initial ListTile design on gamedetails

--- lib/gamedetails.dart
+++ lib/gamedetails.dart
@@ -2,12 +2,25 @@ import 'package:flutter/material.dart';
 import 'appUI.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'game.dart';
+import 'package:intl/intl.dart';
 
 class GameDetailsPage extends StatefulWidget {
   // This variable will store the gameid
   // for the specific game we are displaying the details for.
-  // This game id will be used to query for vital game information. 
+  // This game id will be used to query for vital game information.
   final String gameid;
+
+  // var gamedateformatter = new DateFormat('yMMMMEEEEd');
+  // String formattedgamedate = gamedateformatter.format(gamedate);
+
+  // var starttimeformatter = new DateFormat("jm");
+  // String formattedstarttime = starttimeformatter.format(starttime);
+
+  // DateTime endtime =
+  //     snap.data.documents.elementAt(i).data['endtime'].toDate();
+  // endtime = endtime.toLocal();
+  // var endtimeformatter = new DateFormat("jm");
+  // String formattedendtime = endtimeformatter.format(endtime);
 
   GameDetailsPage(this.gameid);
   @override
@@ -17,7 +30,7 @@ class GameDetailsPage extends StatefulWidget {
 class _GameDetailsPageState extends State<GameDetailsPage> {
   Game currentgame;
   @override
-Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     // We create the streambuilder here to allow us to constantly listen in to changes to the Games
     // database. The materialApp is wrapped inside the streambuilder so we can update data that is used
     // in the materialApp.
@@ -34,36 +47,78 @@ Widget build(BuildContext context) {
           // Any time the snapshot has new data, update the markerlsit
           if (snapshot.hasData) {
             AsyncSnapshot<DocumentSnapshot> snap = snapshot;
-           currentgame = new Game(id: widget.gameid, 
-           userid: snap.data['userid'].toString(),
-           address: snap.data['address'],
-           endtime: snap.data['endtime'],
-           location: snap.data['location'],
-           note: snap.data['note'],
-           playersneeded: snap.data['playersneeded'],
-           private: snap.data['private'],
-           sport: snap.data['sport'],
-           starttime: snap.data['starttime'] );
-           
+            currentgame = new Game(
+                id: widget.gameid,
+                userid: snap.data['userid'].toString(),
+                address: snap.data['address'],
+                endtime: snap.data['endtime'],
+                location: snap.data['location'],
+                note: snap.data['note'],
+                playersneeded: snap.data['playersneeded'],
+                private: snap.data['private'],
+                sport: snap.data['sport'],
+                starttime: snap.data['starttime']);
           } else {
             // Show this loading map screen when we are loading in the database data
             return MaterialApp(
-                  home: Container(
-                    child: Center(
-                      child: Text(
-                        'loading map..',
-                        style: TextStyle(
-                            fontFamily: 'Avenir-Medium',
-                            color: Colors.grey[400]),
-                      ),
-                    ),
+              home: Container(
+                child: Center(
+                  child: Text(
+                    'loading map..',
+                    style: TextStyle(
+                        fontFamily: 'Avenir-Medium', color: Colors.grey[400]),
                   ),
+                ),
+              ),
             );
           }
           return MaterialApp(
+              title: "Game Detail Page",
               // If the initial position is null, return a container saying that we are loading the map.
-              home: Text(currentgame.sport),
-          );
+              home: Scaffold(
+                appBar: AppBar(
+                  title: Text("Game Details"),
+                ),
+                body: Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ListTile(
+                        leading: Icon(Icons.pin_drop),
+                        title: Text("1234 N Main St"),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.person),
+                        title: Text("User123"),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.directions_run),
+                        title: Text(currentgame.sport),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.people),
+                        title: Text(currentgame.playersneeded.toString()),
+                      ),
+                      ListTile(
+                          leading: Icon(Icons.calendar_today),
+                          title: Text("Apr 26, 2020")),
+                      ListTile(
+                        leading: Icon(Icons.timer),
+                        title: Text("5:30 PM"),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.timer_off),
+                        title: Text("6:30 PM"),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.event_note),
+                        title: Text("Bring a ball"),
+                      ),
+                      RaisedButton(onPressed: () {}, child: Text("Join Game"))
+                    ],
+                  ),
+                ),
+              ));
         });
   }
 }
